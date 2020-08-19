c74c4b0ae805d237f57b9efbbd91c7fdaad8dd17
Alexparshh <alexparshh@gmail.com>
2020-08-19 18:41:57 -0400

    fixed tab navigation not updating glitch

--- content_script.js
+++ content_script.js
@@ -1,20 +1,17 @@
-
-window.onload = function () {
-    var title = document.getElementsByClassName("title style-scope ytd-video-primary-info-renderer");
-    title = title[0].innerHTML.split(">")[1].split("<")[0];
-
-
-    chrome.runtime.onMessage.addListener(
-        function(message, sender, sendResponse) {
-            switch(message.type) {
-                case "getTitle":
-                    sendResponse(title);
-                    break;
-                default:
-                    console.error("Unrecognised message: ", message);
-            }
-        }
-    );
+function title() {
+  let title = document.getElementsByClassName(
+    "title style-scope ytd-video-primary-info-renderer"
+  );
+  title = title[0].innerHTML.split(">")[1].split("<")[0];
+  return title
 }
 
-
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  switch (message.type) {
+    case "getTitle":
+      sendResponse(title());
+      break;
+    default:
+      console.error("Unrecognised message: ", message);
+  }
+});
